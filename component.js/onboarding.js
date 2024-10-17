a64da90da53a2e6ee4f60aca9b151cd8a1cb4fac
Bhoomika-tc <159027874+Bhoomika-tc@users.noreply.github.com>
2024-10-17 19:31:51 +0530

    committed the skeloton structure with template

---
+++ component.js/onboarding.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {View} from 'react-native';
+
+export default onBoarding =() => {
+return(
+<View>
+
+</View>
+);
+};
+
+
+const styles = StyleSheet.create({
+
+
+});
+
